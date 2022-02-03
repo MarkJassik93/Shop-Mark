f117dd184cb3cd759fa1ff6e78fe5f43374c5409
MarkJassik93 <80318669+MarkJassik93@users.noreply.github.com>
2022-02-03 13:28:27 +0200

    Weather

---
+++ Shop/Models/Weather/Direction.cs
@@ -0,0 +1,9 @@
+﻿namespace Shop.Models.Weather
+{
+    public class Direction
+    {
+        public double Degrees { get; set; }
+        public string Localized { get; set; }
+        public string English { get; set; }
+    }
+}
---
+++ Shop/Models/Weather/Wind.cs
@@ -0,0 +1,8 @@
+﻿namespace Shop.Models.Weather
+{
+    public class Wind
+    {
+        public Speed Speed { get; set; }
+        public Direction Direction { get; set; }
+    }
+}
